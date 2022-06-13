787c74579122f23e36f822a991b44f1f327d287d
Dmitriy Datsenko <tokentanki@gmail.com>
2022-06-13 19:54:24 +0300

    dark theme ready

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,12 +1,28 @@
 const toggle = document.querySelector('.toggle');
-let html = document.querySelector('html');
+let body = document.querySelector('body');
+let mainArticle = document.querySelector('.main_article')
+let buttons = document.querySelectorAll('.order-btn');
+let articles = document.querySelectorAll('.article_text');
+let aboutBlocks = document.querySelectorAll('.about_block');
 
 toggle.addEventListener('change', e => {
-    if (e.target.checked) {
-        html.style.setProperty('--background', 'black');
-        html.style.setProperty('--text', 'white');
-    } else {
-        root.style.setProperty('--background', 'white');
-        root.style.setProperty('--text', 'black');
+    if (e.target) {
+        toggleTheme()
     }
 })
+
+function toggleTheme() {
+    body.classList.toggle('dark');
+    mainArticle.classList.toggle('dark');
+
+    buttons.forEach(element => {
+        element.classList.toggle('dark-theme_btn');
+    })
+    articles.forEach(element => {
+        element.classList.toggle('dark-theme-text');
+    })
+    aboutBlocks.forEach(element => {
+        element.classList.toggle('dark-theme_about');
+    })
+
+}
